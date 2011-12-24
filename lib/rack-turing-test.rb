26519dc882de87161d940610ead06cc3532e8509
瀧内 元気 <genki@s21g.com>
2011-12-25 05:21:23 +0900

    First commit

---
+++ lib/rack-turing-test.rb
@@ -0,0 +1,9 @@
+require "rack-turing-test/version"
+
+module Rack
+  module Turing
+    module Test
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack-turing-test/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Turing
+    module Test
+      VERSION = "0.0.1"
+    end
+  end
+end
