284729bf35e916d1b995447ad1c3a4d539f91dc3
Genki Takiuchi <genki@s21g.com>
2013-04-04 00:56:28 +0900

    Added some specs

--- spec/rack_turing_test_spec.rb
+++ spec/rack_turing_test_spec.rb
@@ -9,9 +9,20 @@ describe Rack::TuringTest do
     def user_agent; @ua end
   end
 
+  it "should not detect human as a bot" do
+    ua = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)"
+    req = TestRequest.new ua
+    req.should_not be_bot
+  end
+
   it "should detect New-Sogou-Spider as a bot" do
     ua = "New-Sogou-Spider/1.0 (compatible; MSIE 5.5; Windows 98)"
     req = TestRequest.new ua
+    req.should be_bot
+  end
+
+  it "can handle requests that the ua is nil" do
+    req = TestRequest.new nil
     req.should be_bot
   end
 end
